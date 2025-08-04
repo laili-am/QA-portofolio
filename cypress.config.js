1946b1f7fb5d15f66d16663529b70e439b0fe738
Nur Laili Amalia <nurlailiamalia31@gmail.com>
2025-08-04 20:34:53 +0700

    add mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/report/mochawesome-report",
+    overwrite: true,
+    html: true,
+    json: true,
+    timestamp: "mmddyyyy_HHMMss",
+  },
   e2e: {
     baseUrl: "https://www.saucedemo.com/",
     setupNodeEvents(on, config) {
